16de405b5b12768be387659feb8f45f4ecb2e802
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-09 15:45:36 -0300

    chore: updated image version in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 fission spec init
-fission env create --spec --name acc-data-update-env --image nexus.sigame.com.br/fission-account-update-user-data:0.1.0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
+fission env create --spec --name acc-data-update-env --image nexus.sigame.com.br/fission-account-update-user-data:0.1.1 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
 fission fn create --spec --name acc-data-update-fn --env acc-data-update-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
 fission route create --spec --name acc-data-update-rt --method PUT --url /account/update_user --function acc-data-update-fn
